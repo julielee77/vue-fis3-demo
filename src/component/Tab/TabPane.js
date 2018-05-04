b5417c7e1fe70b97a7d234391055dfaf19f14aa0
lihaizhu <lihaizhu@baidu.com>
2018-05-04 17:52:45 +0800

    init tab component

---
+++ src/component/Tab/TabPane.js
@@ -0,0 +1,23 @@
+/*
+ * @file TabPane component
+ * @author github.com/julielee77
+ * @since 2018/05/04
+ */
+
+import Vue from 'vue';
+
+export default {
+    name: 'tab-pane',
+
+    data: {
+        return {};
+    },
+
+    props: {
+
+    },
+
+    methods: {
+        
+    }
+} 
